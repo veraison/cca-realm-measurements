224130e43f1044bbd117ae6beee459a45989df67
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-10-18 13:41:19 +0100

    vmm: Return precise errors
    To use this as a library, return precise errors. May help users handle
    errors better.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- src/vmm.rs
+++ src/vmm.rs
@@ -1,7 +1,18 @@
 use std::fs::File;
 use std::io::{Read, Seek, Write};
 
-use anyhow::{bail, Context, Result};
+#[derive(Debug, thiserror::Error)]
+pub enum VmmError {
+    #[error("invalid Linux header")]
+    InvalidLinuxHeader,
+
+    #[error("file {filename} error: {e}")]
+    File { e: std::io::Error, filename: String },
+
+    #[error("I/O error")]
+    IO(#[from] std::io::Error),
+}
+type Result<T> = core::result::Result<T, VmmError>;
 
 pub type GuestAddress = u64;
 
@@ -30,12 +41,20 @@ impl VmmBlob {
     pub fn from_file(filename: &str, guest_start: GuestAddress) -> Result<VmmBlob> {
         let filename = String::from(filename);
 
-        let file = File::open(&filename)
-            .with_context(|| format!("failed to open file {filename}"))?;
+        let file = File::open(&filename).map_err(|e| VmmError::File {
+            e,
+            filename: filename.clone(),
+        })?;
 
         Ok(VmmBlob {
             guest_start,
-            size: file.metadata()?.len(),
+            size: file
+                .metadata()
+                .map_err(|e| VmmError::File {
+                    e,
+                    filename: filename.clone(),
+                })?
+                .len(),
             load_size: None,
             filename: Some(filename),
             data: BlobStorage::File(file),
@@ -70,14 +89,31 @@ struct LinuxArm64Header {
     res5: u32,
 }
 
-fn read_hdr_u32(file: &mut File) -> Result<u32> {
+impl LinuxArm64Header {
+    fn read(file: &mut File) -> std::io::Result<Self> {
+        Ok(LinuxArm64Header {
+            code0: read_hdr_u32(file)?,
+            code1: read_hdr_u32(file)?,
+            text_offset: read_hdr_u64(file)?,
+            load_size: read_hdr_u64(file)?,
+            flags: read_hdr_u64(file)?,
+            res2: read_hdr_u64(file)?,
+            res3: read_hdr_u64(file)?,
+            res4: read_hdr_u64(file)?,
+            magic: read_hdr_u32(file)?,
+            res5: read_hdr_u32(file)?,
+        })
+    }
+}
+
+fn read_hdr_u32(file: &mut File) -> std::io::Result<u32> {
     let mut b = [0; 4];
 
     file.read_exact(&mut b)?;
     Ok(u32::from_le_bytes(b))
 }
 
-fn read_hdr_u64(file: &mut File) -> Result<u64> {
+fn read_hdr_u64(file: &mut File) -> std::io::Result<u64> {
     let mut b = [0; 8];
 
     file.read_exact(&mut b)?;
@@ -98,35 +134,36 @@ pub fn load_kernel(filename: &str, guest_start: GuestAddress) -> Result<VmmBlob>
         _ => unreachable!(),
     };
 
-    let header = LinuxArm64Header {
-        code0: read_hdr_u32(file)?,
-        code1: read_hdr_u32(file)?,
-        text_offset: read_hdr_u64(file)?,
-        load_size: read_hdr_u64(file)?,
-        flags: read_hdr_u64(file)?,
-        res2: read_hdr_u64(file)?,
-        res3: read_hdr_u64(file)?,
-        res4: read_hdr_u64(file)?,
-        magic: read_hdr_u32(file)?,
-        res5: read_hdr_u32(file)?,
-    };
+    let header = LinuxArm64Header::read(file).map_err(|e| VmmError::File {
+        filename: filename.to_string(),
+        e,
+    })?;
 
     // TODO: decompress a bz2 image, since QEMU supports that.
     // For now expect a decompressed image.
     if header.magic != 0x644d5241 {
-        bail!("invalid Linux header");
+        return Err(VmmError::InvalidLinuxHeader);
     }
 
     blob.load_size = Some(header.load_size);
     blob.guest_start += header.text_offset;
 
-    file.rewind()?;
+    file.rewind().map_err(|e| VmmError::File {
+        e,
+        filename: filename.to_string(),
+    })?;
     Ok(blob)
 }
 
 /// Write generated DTB to file
 pub fn write_dtb(output_dtb: &String, bytes: &[u8]) -> Result<()> {
-    let mut file = File::create(output_dtb)?;
-    file.write_all(bytes)?;
+    let mut file = File::create(output_dtb).map_err(|e| VmmError::File {
+        e,
+        filename: output_dtb.to_string(),
+    })?;
+    file.write_all(bytes).map_err(|e| VmmError::File {
+        e,
+        filename: output_dtb.to_string(),
+    })?;
     Ok(())
 }
